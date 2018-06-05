a5488e7ec65491db75d6b8d0c2a93a62e57dc320
Wikum <wikumdina@gmail.com>
2018-06-05 15:15:39 -0400

    Copying

---
+++ R/divergence.R
@@ -0,0 +1,476 @@
+
+###
+### divergence computation functions
+###
+
+### ====================================================
+### quantiles
+### ====================================================
+
+quantileTransform = function(x){
+  
+  #if(rank_samples)
+  #  x = rank(x, ties.method="average")
+  
+  # start with rank 0, i.e. all values with min(x) will have rank 0
+  # the percentile transformation depends on the non-zero ranks
+  x = x - min(x)
+  
+  y = x
+  # separate out the positive entries and apply transformation
+  z = rank(x[x > 0], ties.method="min")
+  y[x > 0] = (z - min(z))/sum(x > 0)
+  y
+}
+
+
+### apply quantiles transformation to a matrix
+###     rank_samples: TRUE if ranking should be applied before converting to percentiles
+getQuantileMat = function(Mat){
+  
+  newMat = apply(Mat, 2, function(x) quantileTransform(x))
+  
+  rownames(newMat) = rownames(Mat)
+  colnames(newMat) = colnames(Mat)
+  
+  newMat
+}
+
+### ====================================================
+### get expected proportion of divergent feature per sample
+### ====================================================
+
+getAlpha = function(Mat, Ranges){
+  
+  mean(colSums(abs(computeTernary(Mat=Mat, R=Ranges)))/nrow(Mat))
+  
+}
+
+### ====================================================
+### computing ranges
+### ====================================================
+
+computeSingleRange = function(x, gamma, beta, method="partial", j=NULL){
+  
+
+  if(is.null(j))
+    j = max(floor(gamma * length(x)), 1)
+  
+  # get distance to j'th nearest neighbor from each point in x
+  if(method=="fullsort"){
+    xjn = sapply(1:length(x), function(i){
+      sort(abs(x[-i]-x[i]))[j]
+    })
+  }else if(method=="partial"){
+    xjn = sapply(1:length(x), function(i){
+      sort(abs(x[-i]-x[i]), partial=j)[j]
+    })
+  }else if(method=="kdtree"){
+    #xjn = sapply(1:length(x), function(i) 
+    #  max(nn2(data=matrix(x[-i], ncol=1), query=c(x[i]), k=j, eps=0)$nn.dists) )
+    
+    w = WKNNF(x)
+    xjn = sapply(1:length(x), function(i) 
+      max( w$query(x[i], k=j+1, eps=0)$nn.dists ))
+    
+  }else if(method=="quantile_dist"){
+    xjn = apply(as.matrix(dist(x, method="euclidean", diag=FALSE)), 1, function(y) 
+      quantile(y, probs=gamma))
+  }else if(method=="ranks"){
+    xjn = sapply(1:length(x), function(i){
+      y = abs(x[-i]-x[i])
+      y[which(rank(y, ties.method="random") == j)]
+    })
+  }
+  
+  sel = which(xjn <= quantile(xjn, beta))
+  
+  x = x[sel]
+  xjn = xjn[sel]
+  
+  r = c(
+    max(x[which.min(x)] - xjn[which.min(x)], 0),
+    min(x[which.max(x)] + xjn[which.max(x)], 1)
+  )
+  
+  list(range=r, support=sel)
+  
+}
+
+getRangeList = function(Mat, gamma=0.1, beta=0.95, method="partial", par=TRUE,
+                         nmax=200, mmax=1000, verbose=TRUE){
+
+	L = NULL
+
+  # nmax = cols
+  # mmax = rows
+
+  # for testing:
+  # nmax = 50
+
+	if(par){
+
+		require(parallel)
+
+		if(nrow(Mat) > mmax && ncol(Mat) > nmax){
+
+			# for large matrices, break-up into chunks and process
+			# each setion separately with mclapply
+
+			L = getRangesBySections(Mat=Mat, gamma=gamma, beta=beta, method=method,
+    			par=par, nmax=nmax, mmax=mmax, verbose=verbose)
+
+		}else{
+
+			# process in parallel by each row
+
+			L = mclapply(1:nrow(Mat), function(i){
+      	tempx = NULL
+      	tryCatch({
+        	tempx = computeSingleRange(Mat[i, ], gamma=gamma, beta=beta, method=method, j=NULL)
+      	}, error = function(e){warning(e)})
+      	tempx
+    	}, mc.cores=detectCores())
+
+    	if(sum(sapply(L, is.null)) > 0 || length(L) < nrow(Mat)){
+
+    		# did not retrurn all features; re-run without parallel
+        if(verbose)
+      		cat("Not all features returned; re-running without parallelization\n")
+
+    		L = getRangeList(Mat=Mat, gamma=gamma, beta=beta, method=method,
+    			par=FALSE, nmax=nmax, mmax=mmax)
+
+    	}
+
+		}
+
+	}else{
+
+		# run without any parallelization
+
+		L = lapply(1:nrow(Mat), function(i){
+      	tempx = NULL
+      	tryCatch({
+        	tempx = computeSingleRange(Mat[i, ], gamma=gamma, beta=beta, method=method, j=NULL)
+      	}, error = function(e){warning(e)})
+      	tempx
+    	})
+
+	}
+
+	names(L) = rownames(Mat)
+
+	L
+
+}
+
+getRangesBySections = function(Mat, gamma=0.1, beta=0.95, method="partial", par=TRUE,
+                         nmax=200, mmax=1000, verbose=TRUE){
+
+  dirnum = paste(sapply(1:5, function(i) sample(0:9, 1)), collapse="")
+  dirname = sprintf("ranges_%s", dirnum)
+
+  if(dir.exists(dirname)){
+    dirnum = paste(sapply(1:5, function(i) sample(0:9, 1)), collapse="")
+    dirname = sprintf("ranges_%s", dirnum)
+  }
+
+  if(verbose)
+    cat(sprintf("Creating directory %s for saving files\n", dirname))
+
+  if(dir.exists(dirname)){
+    cat(sprintf("Directory %s already exits. Contents will be overwritten.\n", dirname))
+    tryCatch({
+      system(sprintf("rm %s/*.rda", dirname))
+    }, error = function(e){print(e)})
+  }else{
+    dir.create(dirname)
+  }
+
+  # apply to each feature in the baseline data matrix
+
+  nc = detectCores()
+  #nc = floor(detectCores()/2)
+  
+  n = nrow(Mat)
+  k = ceiling(n/mmax)
+  
+  slots = lapply(1:k, function(i){ 
+    x = 1:mmax + ((i-1) * mmax) 
+    x[x <= n]
+  })
+  
+  #print(sapply(slots, function(x) c(x[1:2], x[(length(x)-1):length(x)], length(x)) ))
+  
+  for(j in 1:length(slots)){
+    
+    if(verbose)
+    	cat(sprintf("Processing features %d to %d\n", min(slots[[j]]), max(slots[[j]]) ))
+    
+    partialRanges = getRangeList(Mat[slots[[j]], ], gamma=gamma, beta=beta, method=method, par=par, nmax=nmax, mmax=mmax)
+    
+    if(verbose)
+      cat("Saving..\n")
+    save(partialRanges, file=sprintf("%s/%d.rda", dirname, j))
+    
+    rm(partialRanges)
+    gc()
+  
+  }
+  
+  # assemble
+  if(verbose) 
+    cat("Assembling..\n")
+
+  L = list()
+  for(j in 1:length(slots)){
+    
+    ll = load(sprintf("%s/%d.rda", dirname, j))
+    
+    L[[j]] = partialRanges
+    
+    rm(list=ll)
+    gc()
+    
+  }
+
+  rL = Reduce(c, L)
+  if(! all(names(rL) == rownames(Mat))){
+      warning("Warning: rownames after assembly not in correct order; will try to re-order them..\n")
+      tryCatch({
+        rL = rL[rownames(Mat)]
+      }, error = function(e){print(e)})
+  }
+
+  rL
+}
+
+computeRanges = function(Mat, gamma=0.1, beta=0.95, method="partial", parallel=TRUE, verbose=TRUE){
+  
+  stopifnot(is.matrix(Mat))
+
+  if((! is.numeric(gamma)) || (gamma <= 0) || (gamma >= 1) ){
+    warning("Invalid gamma value; using gamma=0.1\n")
+    gamma = 0.1
+  }
+
+  if((! is.numeric(beta)) || (beta <= 0) || (beta >= 1) ){
+    warning("Invalid beta value; using beta=0.95\n")
+    beta = 0.95
+  }
+
+	if(method == "kdtree"){
+		if("nabor" %in% installed.packages())
+			library("nabor")
+		else{
+			warning("Package nabor for computing kd-trees not found; hence method=partial will be used")
+			method = "partial"
+		}
+	}
+
+  if(verbose){
+    cat(sprintf("Computing ranges from %d reference samples for %d features\n", 
+                ncol(Mat), nrow(Mat)))
+    cat(sprintf("[beta=%.3f, gamma=%.3f]\n", beta, gamma))
+  }
+
+  # apply to each feature in the baseline data matrix
+  L = getRangeList(Mat=Mat, gamma=gamma, beta=beta, method=method, par=parallel, verbose=verbose)
+  
+  R = data.frame(t(sapply(L, function(x) x$range)))
+  colnames(R) = c("baseline.low", "baseline.high")
+  
+  S = t(sapply(L, function(x) 1*(1:ncol(Mat) %in% x$support) ))
+  colnames(S) = colnames(Mat)
+  
+  alpha=getAlpha(Mat=Mat, Ranges=R)
+  if(verbose)
+    cat(sprintf("[Expected proportion of divergent features per sample=%.3f]\n", alpha))
+  
+  list(Ranges=R, 
+       Support=S,
+       gamma=gamma,
+       alpha=alpha)
+  
+}
+
+### ====================================================
+### search for gamma
+### ====================================================
+
+findGamma = function(Mat,
+	gamma=c(1:9/100, 1:9/10),
+  beta=0.95, 
+  alpha=0.01,
+  method="partial", 
+  parallel=TRUE,
+  verbose=TRUE){
+ 
+  stopifnot(is.matrix(Mat))
+
+ if(verbose)
+    cat(sprintf("Searching optimal gamma for alpha=%.5f\n", alpha))
+   
+  optimal_gamma = -1
+  
+  gamma = sort(gamma)
+  names(gamma) = paste("g", 1:length(gamma), sep="")
+  
+  g = c()
+  e = rep(NA, length(gamma))
+  names(e) = names(gamma)
+    
+  RangesList = list()
+  for(i in 1:length(gamma)){
+    L = computeRanges(Mat=Mat, gamma=gamma[i], beta=beta, method=method, parallel=parallel, verbose=verbose)
+    
+    RangesList[[i]] = L
+    e[i] = L$alpha
+    
+    g = c(g, gamma[i])
+    if(e[i] <= alpha)
+      break
+  }
+  names(RangesList) = names(gamma)[1:length(RangesList)]
+  names(g) = names(gamma)[1:length(g)]
+
+  optimal = FALSE
+  if(length(which(e <= alpha)) < 1){
+    sel = which.max(g)
+  }else{
+    sel = which(g == min(g[which(e <= alpha)]))
+  }
+  optimal_gamma = g[sel]
+  R_star = RangesList[[ sel ]]
+  e_star = e[sel]
+  
+  if(e_star <= alpha)
+    optimal = TRUE
+  
+  temp_e = e
+  names(temp_e) = g
+  #print(temp_e)
+
+  if(verbose)
+    cat(sprintf("Search results for alpha=%.5f: gamma=%.5f, expectation=%.5f, optimal=%s\n", alpha, optimal_gamma, e_star, optimal))
+  
+  list(Ranges=R_star$Ranges, 
+       Support=R_star$Support,
+       gamma=optimal_gamma,
+       alpha=e_star,
+       optimal=optimal,
+       alpha_space=data.frame(gamma=gamma, alpha=e)
+       )
+  
+}
+
+### ====================================================
+### compute ternary form
+### ====================================================
+
+computeTernary = function(Mat, R){
+  
+  lower = "baseline.low"
+  upper = "baseline.high"
+
+  if(nrow(Mat) != nrow(R))
+    cat(sprintf("WARNING [%d, %d]\n", nrow(Mat), nrow(R)))
+  
+  DMat = ((Mat < R[, lower]) * (-1)) + ((Mat > R[, upper]) * 1)
+  rownames(DMat) = rownames(Mat)
+  colnames(DMat) = colnames(Mat)
+  DMat
+  
+}
+
+### ====================================================
+### compute divergences
+### ====================================================
+
+computeTernaryDigitization = function(Mat, baseMat, 
+                              computeQuantiles=TRUE,
+                              gamma=c(1:9/100, 1:9/10),
+                              beta=0.95, 
+                              alpha=0.01,
+                              method="partial",
+                              parallel=TRUE,
+                              verbose=TRUE,
+                              findGamma=TRUE, 
+                              Groups=NULL,                             
+                              classes=NULL){
+
+  stopifnot(rownames(Mat) == rownames(baseMat))
+
+  if(! is.null(Groups)){
+
+    stopifnot(length(Groups) == ncol(Mat))
+
+    if(! is.factor(Groups))
+      Groups = factor(Groups)
+
+    if(is.null(classes))
+       classes = levels(Groups)
+ 
+  }
+
+  if(! is.numeric(alpha)){
+    warning("Invalid alpha value\n")
+    findGamma = FALSE
+  }else if(alpha < 0 || alpha > 1){
+    warning("Invalid alpha value\n")
+    findGamma = FALSE
+  }
+
+  if(computeQuantiles){
+    if(verbose)
+      cat(sprintf("Computing quantiles..\n"))
+    baseMat = getQuantileMat(baseMat)
+    Mat = getQuantileMat(Mat)
+  }
+
+  if(findGamma){
+    L = findGamma(Mat=baseMat, gamma=gamma, beta=beta, alpha=alpha, method=method, parallel=parallel, verbose=verbose)
+  }
+  else{
+    if(verbose)
+      cat(sprintf("Using gamma=.4f\n", gamma[1]))
+    L = findGamma(Mat=baseMat, gamma=gamma[1], beta=beta, alpha=alpha, method=method, parallel=parallel, verbose=FALSE)
+  }
+
+  DMat_ternary = computeTernary(Mat=Mat, R=L$Ranges)
+  
+  baseMat_ternary = computeTernary(Mat=baseMat, R=L$Ranges)
+
+  DMat = abs(DMat_ternary)
+  
+  D = rowMeans(DMat)
+  N = colSums(DMat)
+  
+  Npos = colSums(DMat_ternary > 0)
+  Nneg = colSums(DMat_ternary < 0)
+  
+  df = data.frame(feature=rownames(Mat), prob.div=D)
+
+  if(! is.null(Groups)){
+
+    classDiv = sapply(classes, function(x) rowMeans(DMat[, which(Groups == x)]))
+
+    df = data.frame(df, classDiv)
+    colnames(df) = c("feature", "prob.div", paste("prob.div.", classes, sep=""))
+
+  }
+
+  list(Mat.div=DMat_ternary,
+      baseMat.div = baseMat_ternary,
+      div = data.frame(sample=colnames(Mat), count.div=N, count.div.upper=Npos, count.div.lower=Nneg),
+      features.div = df,
+      Ranges = L$Ranges,
+      Support = L$Support,
+      gamma = L$gamma,
+      alpha = L$alpha,
+      optimal = L$optimal,
+      alpha_space = L$alpha_space)
+    
+}
+
---
+++ R/main.R
@@ -0,0 +1,264 @@
+#' Compute quantile transformations
+#'
+#' Function for computing the quantile transformation for one or more samples
+#' supplied as columns of a matrix.
+#'
+#' @param Mat Matrix of data, with each column corresponding to a sample and each row corresponding to a feature.
+#'
+#' @return A matrix of the same dimensions as Mat with the quantile data.
+#'
+#' @export
+#'
+#' @examples
+#' data(divergence)
+#' quantileMat = computeQuantileMatrix(breastTCGA_Mat)
+#'
+
+computeQuantileMatrix <- function(Mat){
+	getQuantileMat(Mat=Mat)
+}
+
+# ==================================================================================================
+#' Estimate the baseline support
+#'
+#' Function for computing the basline support for univariate features given gamma
+#' and beta parameters.
+#'
+#' @param Mat Matrix of data, with each column corresponding to a sample and each 
+#' row corresponding to a feature
+#' @param gamma Parameter for selecting radius around each support point (0 < gamma < 1).
+#' By default gamma = 0.1.
+#' @param beta Parameter for eliminating outliers (0 < beta < 1). By default beta=0.95.
+#' @param method Method of computing distance to j'th nearest neighbour. Options are 
+#' "partial" (default option and usually the fastest) for using partial quicksort, 
+#' "fullsort" for using full quicksort, "kdtree" for using a kd-tree (requires the package nabor), 
+#' "quantile_dist" for computing quantiles over pairwise distances, "ranks" for using the rank function.
+#' @param parallel Logical indicating whether to compute features parallelly with mclapply (defaults to TRUE).
+#' @param verbose Logical indicating whether to print status related messages during computation (defaults
+#'  to TRUE).
+#'
+#' @return A list with elements "Ranges" (a data frame containing the baseline interval for each feature),
+#' "Support" (a binary matrix of the same dimensions as Mat indicating whether each sample was a support 
+#' for a feature or not, 1=support, 0=not in the support), and "alpha" (the expected number of divergent 
+#' features per sample estimated over the samples).
+#' 
+#' @keywords baseline, support
+#' @export
+#' 
+#' @examples
+#' data(divergence)
+#' S = computeUnivariateSupport(computeQuantileMatrix(breastTCGA_Mat[, breastTCGA_Group=="NORMAL"]))
+#'
+
+computeUnivariateSupport <- function(Mat, gamma=0.1, beta=0.95, method="partial", parallel=TRUE, verbose=TRUE){
+	computeRanges(Mat=Mat, gamma=gamma, beta=beta, method=method, parallel=parallel, verbose=verbose)
+}
+
+# ==================================================================================================
+#' Search for optimal gamma and associated support
+#'
+#' Function for searching through a range of gamma values for finding the smallest gamma that 
+#' provides expected proportion of divergent features per sample less than or equal to alpha.
+#'
+#' @param Mat Matrix of data, with each column corresponding to a sample and each 
+#' row corresponding to a feature
+#' @param gamma Range of gamma values to search through. 
+#' By default gamma = \{0.01, 0.02, ... 0.09, 0.1, 0.2, ..., 0.9\}.
+#' @param beta Parameter for eliminating outliers (0 < beta < 1). By default beta=0.95.
+#' @param alpha Expected proportion of divergent features per sample to be estimated
+#' over the samples in Mat. By default alpha = 0.01; i.e. search for the smallest gamma that provides
+#' 1\% or less number of divergent features per sample. 
+#' @param method Method of computing distance to j'th nearest neighbour. Options are 
+#' "partial" (default option and usually the fastest) for using partial quicksort, 
+#' "fullsort" for using full quicksort, "kdtree" for using a kd-tree (requires the package nabor), 
+#' "quantile_dist" for computing quantiles over pairwise distances, "ranks" for using the rank function.
+#' @param parallel Logical indicating whether to compute features parallelly with mclapply (defaults to TRUE).
+#' @param verbose Logical indicating whether to print status related messages during computation (defaults
+#'  to TRUE).
+#'
+#' @return A list with elements "Ranges" (a data frame containing the baseline interval for each feature for the),
+#' selected gamma, "Support" (a binary matrix of the same dimensions as Mat indicating whether each sample was a support 
+#' for a feature or not, 1=support, 0=not in the support), "gamma" (selected gamma value), "alpha" (the expected number
+#'  of divergent features per sample computed over the baseline data matrix), "optimal" (logical indicaing whether the 
+#' selected gamma value provided the necessary alpha requirement), and "alpha_space" (a data frame with alpha values 
+#' for each gamma searched).
+#' 
+#' @keywords gamma
+#' @export
+#' 
+#' @examples
+#' data(divergence)
+#' S = findUnivariateGammaWithSupport(computeQuantileMatrix(breastTCGA_Mat[, breastTCGA_Group=="NORMAL"]))
+#'
+findUnivariateGammaWithSupport <- function(Mat, gamma=c(1:9/100, 1:9/10), beta=0.95, alpha=0.01, method="partial", parallel=TRUE, verbose=TRUE){
+     findGamma(Mat=Mat, gamma=gamma, beta=beta, alpha=alpha, method=method, parallel=parallel, verbose=verbose)
+}
+
+# ==================================================================================================
+#'
+#' Compute the ternary matrix with digitized divergence coding
+#'
+#' Function for obtaining the ternary form for a matrix of data given a baseline range
+#'
+#' @param Mat Matrix of data to be digitized (usually in quantiles), with each column corresponding to a sample and each 
+#' row corresponding to a feature
+#' @param Ranges A data frame containing the baseline range of each features; this corresponds to the 'Ranges' element of 
+#' the output of findUnivariateGammaWithSupport() and computeUnivariateSupport()
+#'
+#' @return A matrix with the same dimensions as Mat containing the ternary form data.
+#'
+#' @keywords ternary digitization
+#' @export
+#'
+#' @examples
+#' data(divergence)
+#' S = computeUnivariateSupport(computeQuantileMatrix(breastTCGA_Mat[, breastTCGA_Group=="NORMAL"]))
+#' T = computeUnivariateTernaryMatrix(Mat=computeQuantileMatrix(breastTCGA_Mat[, breastTCGA_Group!="NORMAL", ]), Ranges=S$Ranges)
+
+computeUnivariateTernaryMatrix <- function(Mat, Ranges){
+			computeTernary(Mat=Mat, R=Ranges)
+}
+
+# ==================================================================================================
+#' 
+#' Compute ternary digitization
+#'
+#' Function for obtaining the digitized form, along with other relevant statistics and measures 
+#' given a data matrix and a baseline matrix 
+#'
+#' @param Mat Matrix of data to be digitized (usually in quantiles), with each column corresponding to a sample and each 
+#' row corresponding to a feature
+#' @param baseMat Matrix of baseline data (usually in quantiles), with each column corresponding to a sample and each 
+#' row corresponding to a feature
+#' @param computeQuantiles Apply quantile transformation to both data and baseline matrices (TRUE or FALSE; defaults to TRUE).
+#' @param gamma Range of gamma values to search through. 
+#' By default gamma = {0.01, 0.02, ... 0.09, 0.1, 0.2, ..., 0.9}.
+#' @param beta Parameter for eliminating outliers (0 < beta < 1). By default beta=0.95.
+#' @param alpha Expected proportion of divergent features per sample to be estimated. The optimal gamma providing
+#' this level of divergence in the baseline data will be searched for.
+#' @param method Method of computing distance to j'th nearest neighbour. Options are 
+#' "partial" (default option and usually the fastest) for using partial quicksort, 
+#' "fullsort" for using full quicksort, "kdtree" for using a kd-tree (requires the package nabor), 
+#' "quantile_dist" for computing quantiles over pairwise distances, "ranks" for using the rank function.
+#' @param parallel Logical indicating whether to compute features parallelly with mclapply (defaults to TRUE).
+#' @param verbose Logical indicating whether to print status related messages during computation (defaults
+#'  to TRUE).
+#' @param findGamma Logical indicating whether to search for optimal gamma values through the given gamma values (defaults to 
+#' TRUE). If FALSE, the first value given in gamma will be used.
+#' @param Groups Factor indicating class association of samples
+#' @param classes Vector of class labels
+#'
+#' @return A list
+#'
+#' @keywords digitize ternary
+#' @export
+#'
+#' @examples
+#' data(divergence)
+#' D = computeDigitization(breastTCGA_Mat[, breastTCGA_Group=="NORMAL"], breastTCGA_Mat[, breastTCGA_Group!="NORMAL"])
+#'
+
+computeUnivariateDigitization <- function(Mat, baseMat, 
+															computeQuantiles=TRUE,
+                              gamma=c(1:9/100, 1:9/10),
+                              beta=0.95, 
+                              alpha=0.01,
+                              method="partial",
+                              parallel=TRUE,
+                              verbose=TRUE,
+                              findGamma=TRUE, 
+                              Groups=NULL,                             
+                              classes=NULL
+){
+
+	computeTernaryDigitization(Mat=Mat, baseMat=baseMat, computeQuantiles=computeQuantiles,
+		gamma=gamma, beta=beta,
+		alpha=alpha, method=method,
+		parallel=parallel, verbose=verbose, 
+		findGamma=findGamma, Groups=Groups, classes=classes)
+
+}
+
+# ==================================================================================================
+#'
+#' Compute chi-squared test
+#'
+#' Given a binary or ternary data matrix with class associations of samples, computes chi-squared tests
+#' for each feature between two given classes
+#'
+#' @param Mat Matrix of digitized binary or ternary data with each column corresponding to a sample and each 
+#' row corresponding to a feature
+#' @param Groups Factor indicating class association of samples
+#' @param classes Vector of class labels; the test will be applied between the first two classes given.
+#'
+#' @keywords chi-squared
+#' @export
+#'
+
+computeChiSquaredTest <- function(Mat, Groups, classes){
+
+	chiSquaredTest(Mat=Mat, Groups=Groups, classes=classes)
+
+}
+
+
+# ==================================================================================================
+#' Compute the support for a list of feature sets
+#' @export
+
+computeMultivariateSupport <- function(Mat, genesets, gamma=1:9/10, beta=0.95, method="euclidean"){
+
+	if(is.list(genesets))
+		genematrix = geneset_list_to_matrix(genesets)
+	else
+		genematrix = genesets
+
+	computeMultiGenesetSupport(Mat, genematrix, gamma, beta, method)
+}
+
+# ==================================================================================================
+#' Find optimal gamma and corresponding support for list of feature sets
+#' @export
+
+findMultivariateGammaAndSupport <-function(Mat, genesets, gamma=1:9/10, beta=0.95, alpha=0.01, method="euclidean"){
+
+	if(is.list(genesets))
+		genematrix = geneset_list_to_matrix(genesets)
+	else
+		genematrix = genesets
+
+	findMultiSetGammaAndSupport(Mat, genematrix, gamma, beta, alpha, method)
+}
+
+# ==================================================================================================
+#' Compute binary matrix given feature set support
+#' @export
+
+computeMultivariateBinaryMatrix <- function(Mat, genesets, Centermatrix_list, Radius_list, method="euclidean"){
+
+	if(is.list(genesets))
+		genematrix = geneset_list_to_matrix(genesets)
+	else
+		genematrix = genesets
+
+	computeMultiSetBinaryMatrix(Mat, genematrix, Centermatrix_list, Radius_list, method)
+}
+
+# ==================================================================================================
+#' Compute binary digitization for feature sets
+#' @export
+
+computeMultivariateDigitization <- function(Mat, basemat, genesets, gamma=1:9/10, beta=0.95, alpha=0.01, method="euclidean", findGamma =TRUE){
+
+	if(is.list(genesets))
+		genematrix = geneset_list_to_matrix(genesets)
+	else
+		genematrix = genesets
+
+	computeMultiSetDigitization(Mat, basemat, genematrix,gamma,beta, alpha, method="euclidean", findGamma =TRUE)
+}
+
+
+
+
+
